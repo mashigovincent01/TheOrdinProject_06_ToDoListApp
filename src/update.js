6d107637c6ddb6d7402549a06aadd0acd5d8cd97
mashigovincent01 <mashigovincent407@gmail.com>
2024-08-21 22:29:52 +0200

    must fix click events

--- src/update.js
+++ src/update.js
@@ -12,8 +12,30 @@ const updateTasks = (todoList, projectName)=>{
     for(let i = 0; i < myTasks.length; i++){
         tasks.appendChild(taskCard(myTasks[i], i));
     }
+    updateEditIcons();
+    updateDeleteIcons();
 }
 
+function updateEditIcons(){
+    const editIcons = document.querySelectorAll(".edit-icon");
+    console.log(editIcons);
+    editIcons.forEach((icon)=>{
+        icon.addEventListener("click", (e)=>{
+            
+            alert("Hello world" + " " + e.target.id);
+        });
+    });
+}
+function updateDeleteIcons(){
+    const deleteIcons = document.querySelectorAll(".delete-icon");
+    console.log(deleteIcons);
+    deleteIcons.forEach((icon)=>{
+        icon.addEventListener("click", (e)=>{
+            
+            alert("Hello world" + " " + e.target.id);
+        });
+    });
+}
 const updateProjects = (todoList)=>{
     projects.innerHTML = "";
     const myProjects = todoList.getProjects();
